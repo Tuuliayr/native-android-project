3acb0511a8ec4d11882fcca473cc361eee900680
Tuuli <tuuli.ayravainen@tuni.fi>
2021-05-06 15:45:46 +0300

    Working notification and a new view when tapping it.

---
+++ MyProject/app/src/main/java/com/example/myproject/App.kt
@@ -0,0 +1,46 @@
+package com.example.myproject
+
+import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import androidx.annotation.RequiresApi
+
+class App : Application() {
+    companion object {
+        const val CHANNEL_1_ID: String = "channel1"
+        const val CHANNEL_2_ID: String = "channel2"
+    }
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate() {
+        super.onCreate()
+
+        createNotificationChannels()
+    }
+
+    // Create notification channels for android versions greater than than oreo (api level 26)
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannels() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // channel 1
+            val channel1 =
+                    NotificationChannel(CHANNEL_1_ID, "channel1",
+                            NotificationManager.IMPORTANCE_HIGH)
+            // weather channel?
+            channel1.description = "This is channel 1"
+
+            // channel 2
+            val channel2 =
+                    NotificationChannel(CHANNEL_2_ID, "channel2",
+                            NotificationManager.IMPORTANCE_LOW)
+            channel2.description = "This is channel 2"
+
+            // register them
+            val notificationManager = getSystemService(NotificationManager::class.java)
+            //val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel1)
+            notificationManager.createNotificationChannel(channel2)
+        }
+    }
+}
--- MyProject/app/src/main/java/com/example/myproject/MainActivity.kt
+++ MyProject/app/src/main/java/com/example/myproject/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.example.myproject
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.example.myproject.App.Companion.CHANNEL_1_ID
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var notificationManager : NotificationManagerCompat
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        notificationManager = NotificationManagerCompat.from(this)
+        createNotification()
+    }
+
+    fun getURL() {
+
+    }
+
+    private fun createNotification() {
+        // Open activity when notification is tapped
+        val resultIntent = Intent(this, WeatherNotificationActivity::class.java)
+        val resultPendingIntent = PendingIntent.getActivity(this, 1, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        val notification = NotificationCompat.Builder(this, CHANNEL_1_ID)
+                .setSmallIcon(R.drawable.ic_strength)
+                .setContentTitle("Hello")
+                .setContentText("World")
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setAutoCancel(true)
+                .setContentIntent(resultPendingIntent)
+                .build()
+
+        notificationManager.notify(1, notification)
+
+        //val notificationChannelId: String = NotificationUtil.createNotificationChannel(this, bigTextStyleReminderAppData)
     }
 }
